63a174b9d2aef65c268546d989729306c33cfd57
Mohammed Naji Abdullah <121554058+Keolos@users.noreply.github.com>
2025-05-28 13:57:13 +0000

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,4 +1,5 @@
-number1 = 10
-number2 = 5
-number1 + number2    #Addition of 10 and 5 is 15
-
+number1 = int(10)
+number2 = int(5)
+addition = number1 + number2
+#Addition of 10 and 5 is 15
+print(f"Addition of {number1} and {number2} is {addition}")
